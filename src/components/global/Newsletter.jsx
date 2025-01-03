7e4558d09292d7384f21c21bac8f3384a9a1041e
”sagecave” <Andreassage@gmail.com>
2025-01-03 12:30:26 +0100

    newsletter benytter use effekt og virker nu

--- src/components/global/Newsletter.jsx
+++ src/components/global/Newsletter.jsx
@@ -1,25 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
 import { postSub } from "@/lib/supabase";
 import { revalidatePath } from "next/cache";
 import Button from "./buttonFolder/PrimaryButton";
 
 const Newsletter = () => {
-  async function postingSub(formData) {
-    "use server";
+  const [data, setData] = useState([]);
+  function postingSub(formData) {
+    //   "use server";
     const data = {
-      name: formData.get("name"),
+      navn: formData.get("navn"),
       email: formData.get("email"),
     };
-    await postSub(data);
-
-    revalidatePath("/");
+    //   await postSub(data);
+    setData([data]);
+    //   revalidatePath("/");
   }
+  useEffect(() => {
+    if (data === null) {
+      return;
+    }
+
+    console.log("useEffect bliver brugt", data);
+    fetch("https://acrgwzdfrmjzprvhpnuw.supabase.co/rest/v1/subscription", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjcmd3emRmcm1qenBydmhwbnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MzQ3OTUsImV4cCI6MjA0ODExMDc5NX0.Lh-kfAyHHAPYOS3sze8ay1u_KBWbXAR0mtgmKsVEeiU",
+        Prefer: "return=representation",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        return () => console.log("date kommer vel?", data);
+      });
+  }, [data]);
   return (
     <form className="flex flex-col gap-5 items-center " action={postingSub}>
       <div className="flex flex-col  text-customWhite">
-        <label className="font-bold" htmlFor="name">
+        <label className="font-bold" htmlFor="navn">
           Navn
         </label>
-        <input className="bg-customBlack_4 p-2 min-w-64 sm:min-w-96 " autoComplete="given-name" required placeholder="Navn" type="text" id="name" name="name" />
+        <input className="bg-customBlack_4 p-2 min-w-64 sm:min-w-96 " autoComplete="given-name" required placeholder="Navn" type="text" id="navn" name="navn" />
       </div>
       <div className="flex flex-col  text-customWhite">
         <label className="font-bold" htmlFor="email">
