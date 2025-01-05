967ced007debeac076df95860bacfe37180fd9ea
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-05 21:06:40 +0100

    add critical attribute aria-live assertive to error

--- src/components/bookingsystem/Campsite.jsx
+++ src/components/bookingsystem/Campsite.jsx
@@ -13,7 +13,12 @@ const ceasarDressing = Caesar_Dressing({
   display: "swap",
 });
 
-export default function Campsite({ state, formAction, setReservedId, setTimeOut }) {
+export default function Campsite({
+  state,
+  formAction,
+  setReservedId,
+  setTimeOut,
+}) {
   const [spots, setSpots] = useState([]);
   const [cart, setCart] = useContext(CartContext);
   const [twoPersonCount, setTwoPersonCount] = useState(0);
@@ -138,9 +143,18 @@ export default function Campsite({ state, formAction, setReservedId, setTimeOut 
 
   return (
     <div className="flex justify-center mx-4">
-      <motion.form initial={{ opacity: 0, x: 20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.3 }} className=" inline-flex flex-col flex-1 bg-gradient-to-tl border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md">
-        <h2 className={`${ceasarDressing.className} text-3xl text-white`}>HVOR VIL DU CAMPE?</h2>
-        <h3 className="font-bold text-sm mb-4 text-gray-400">VÆLG VENLIGST EN LEDIG CAMPINGOMRÅDE</h3>
+      <motion.form
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3 }}
+        className=" inline-flex flex-col flex-1 bg-gradient-to-tl border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md"
+      >
+        <h2 className={`${ceasarDressing.className} text-3xl text-white`}>
+          HVOR VIL DU CAMPE?
+        </h2>
+        <h3 className="font-bold text-sm mb-4 text-gray-400">
+          VÆLG VENLIGST EN LEDIG CAMPINGOMRÅDE
+        </h3>
         {isLoading && (
           <div className="flex justify-center items-center">
             <span className="w-14 h-14 loader-animation "></span>
@@ -151,9 +165,17 @@ export default function Campsite({ state, formAction, setReservedId, setTimeOut 
             <li
               onClick={() => updateCampsite(spot.area, spot.available)}
               key={i}
-              className={`${spot.available < numPeople && "bg-gray-300 text-gray-500 cursor-not-allowed hidden disabled"}${spot.area === selectedCampsite && "border-4"} bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-2 rounded-md select-none cursor-pointer`}
+              className={`${
+                spot.available < numPeople &&
+                "bg-gray-300 text-gray-500 cursor-not-allowed hidden disabled"
+              }${
+                spot.area === selectedCampsite && "border-4"
+              } bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-2 rounded-md select-none cursor-pointer`}
               style={{
-                borderImage: spot.area === selectedCampsite ? "linear-gradient(to right, #EC2783, #D82023 , #EC4D08) 1" : "none",
+                borderImage:
+                  spot.area === selectedCampsite
+                    ? "linear-gradient(to right, #EC2783, #D82023 , #EC4D08) 1"
+                    : "none",
                 borderRadius: "0.375rem",
                 borderWidth: spot.area === selectedCampsite ? "2px" : "1px",
               }}
@@ -166,56 +188,98 @@ export default function Campsite({ state, formAction, setReservedId, setTimeOut 
 
         <div className="flex flex-col justify-evenly gap-4">
           <section>
-            <h4 className={`${ceasarDressing.className} text-3xl text-white mt-8`}>LEJE AF TELTE</h4>
+            <h3
+              className={`${ceasarDressing.className} text-3xl text-white mt-8`}
+            >
+              LEJE AF TELTE
+            </h3>
             <ul className="my-4 inline-flex flex-col gap-6">
               <li className=" flex-row text-white flex gap-12 bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-4 rounded-md">
                 <div>
-                  <h3 className="font-bold text-xl">2 Personers Telt</h3>
+                  <h4 className="font-bold text-xl">2 Personers Telt</h4>
                   <p className="text-xs font-normal text-gray-300">299kr</p>
                 </div>
-                <CounterInput name="twoPeople" count={twoPersonCount} setCount={updateTwoPersonTentCount} />
+                <CounterInput
+                  name="twoPeople"
+                  count={twoPersonCount}
+                  setCount={updateTwoPersonTentCount}
+                />
               </li>
               <li className="flex flex-row text-white gap-12  bg-gradient-to-tl border border-gray-900 from-customBlack_2 to-customBlack p-4 rounded-md">
                 <div>
-                  <h3 className="font-bold text-xl">3 Personers Telt</h3>
+                  <h4 className="font-bold text-xl">3 Personers Telt</h4>
                   <p className="text-xs font-normal text-gray-300">399kr</p>
                 </div>
-                <CounterInput name="threePeople" max={10} count={threePersonCount} setCount={updateThreePersonTentCount} />
+                <CounterInput
+                  name="threePeople"
+                  max={10}
+                  count={threePersonCount}
+                  setCount={updateThreePersonTentCount}
+                />
               </li>
             </ul>
             {countError && (
-              <p className="text-red-500 flex items-center gap-2">
+              <p className="text-red-500 flex items-center gap-2" aria-live="assertive">
                 <IoIosAlert size={36} /> {countError}
               </p>
             )}
           </section>
           <section>
-            <h3 className={`${ceasarDressing.className} text-3xl text-white mb-4`}>SUPPLEMENT</h3>
+            <h3
+              className={`${ceasarDressing.className} text-3xl text-white mb-4`}
+            >
+              SUPPLEMENT
+            </h3>
 
             <div className="flex items-center p-4 rounded-md bg-gradient-to-tl border border-green-800 from-black to-green-900">
               <div className="flex h-5">
-                <input id="helper-checkbox" aria-describedby="helper-checkbox-text" name="greenCamping" type="checkbox" checked={greenCamping} className="w-6 h-6 bg-gray-100 border-gray-300 rounded-full focus:ring-green-500 dark:ring-offset-gray-800 focus:ring-2 checked:bg-green-600 checked:border-green-600" onChange={updateGreenCamping} />
+                <input
+                  id="helper-checkbox"
+                  aria-describedby="helper-checkbox-text"
+                  name="greenCamping"
+                  type="checkbox"
+                  checked={greenCamping}
+                  className="w-6 h-6 bg-gray-100 border-gray-300 rounded-full focus:ring-green-500 dark:ring-offset-gray-800 focus:ring-2 checked:bg-green-600 checked:border-green-600"
+                  onChange={updateGreenCamping}
+                />
               </div>
               <div className="ms-2 text-sm">
-                <label htmlFor="helper-checkbox" className="font-bold inline-flex gap-2 items-baseline text-xl text-white">
+                <label
+                  htmlFor="helper-checkbox"
+                  className="font-bold inline-flex gap-2 items-baseline text-xl text-white"
+                >
                   Grøn Camping <FaLeaf className="text-green-400" />
                 </label>
-                <p id="helper-checkbox-text" className="text-xs font-normal text-gray-300">
+                <p
+                  id="helper-checkbox-text"
+                  className="text-xs font-normal text-gray-300"
+                >
                   249kr
                 </p>
-                <p className="font-light text-sm text-gray-100 w-64">Perfekt til dig, der ønsker en mere miljøvenlig festivaloplevelse.</p>
+                <p className="font-light text-sm text-gray-100 w-64">
+                  Perfekt til dig, der ønsker en mere miljøvenlig
+                  festivaloplevelse.
+                </p>
               </div>
             </div>
           </section>
         </div>
         <div className="flex flex-col items-end">
           {handleError && (
-            <p className="text-red-500 flex items-center gap-2">
+            <p className="text-red-500 flex items-center gap-2" aria-live="assertive">
               <IoIosAlert size={36} />
               {handleError}
             </p>
           )}
-          <button className={`${selectedCampsite ? "font-semibold py-2 rounded-sm px-8 my-8 ml-auto text-xl bg-gradient-to-bl from-customPink text-white to-customOrange text-transparent w-full sm:w-auto" : "bg-gray-500 rounded-sm px-8 py-2 my-8 text-xl ml-auto font-bold text-gray-300 cursor-not-allowed"}`} formAction={handleNext} type="submit">
+          <button
+            className={`${
+              selectedCampsite
+                ? "font-semibold py-2 rounded-sm px-8 my-8 ml-auto text-xl bg-gradient-to-bl from-customPink text-white to-customOrange text-transparent w-full sm:w-auto"
+                : "bg-gray-500 rounded-sm px-8 py-2 my-8 text-xl ml-auto font-bold text-gray-300 cursor-not-allowed"
+            }`}
+            formAction={handleNext}
+            type="submit"
+          >
             Næste
           </button>
         </div>
