746acf93f6948c1abc1c8cb9068b0f2b1f1e856a
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-05 21:14:13 +0100

    see deployment

--- src/components/bookingsystem/CounterInput.jsx
+++ src/components/bookingsystem/CounterInput.jsx
@@ -34,43 +34,46 @@ const CounterInput = ({ count, setCount, max, name }) => {
 
   return (
     <>
-      <label htmlFor="numberInput" className="sr-only">
-        Antal Billetter
-      </label>
-      {/* Fortæller her at vi gerne vil gruppere vores elementer som samlet input.
+      <label htmlFor="numberInput">
+        {/* Fortæller her at vi gerne vil gruppere vores elementer som samlet input.
        Vi har inde i vores Card givet en property 'name' der kan adskille om det så er en vip eller single billet */}
-      <div className="flex item-center max-w-[8rem]" role="group" aria-labelledby={`counter-${name}-label`}>
-        <button
-          aria-label="Minsk Antal Billetter"
-          onClick={subtractQuantity}
-          type="button"
-          className="appearance-textfield bg-gray-950 hover:bg-gray-900 border border-gray-800 p-3 h-11 rounded-s-lg"
-          >
-          <FiMinus />
-        </button>
-        <input
-        aria-labelledby={`counter-${name}-label`}
-          name={name}
-          value={count}
-          onChange={() => {}}
-          type="number"
-          required
-          // Informere brugeren opdateringen
-          aria-live="polite"
-          className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none appearance-textfield disabled-none text-white text-center bg-gray-950 border-y border-gray-800 h-11 py-2.5 w-full"
-          />
-        <button
-          aria-label="Øg Antal Billetter"
-          onClick={addQuantity}
-          type="button"
-          className=" appearance-textfield bg-gray-950 hover:bg-gray-900 border border-gray-800 p-3 h-11 rounded-e-lg"
+        <div
+          className="flex item-center max-w-[8rem]"
+          role="group"
+          aria-labelledby={`counter-${name}-label`}
         >
-          <FiPlus />
-        </button>
-      </div>
-      <p id={`counter-${name}-label`} className="sr-only">
-        Du kan maks købe {max} billetter.
-      </p>
+          <button
+            aria-label="Minsk Antal Billetter"
+            onClick={subtractQuantity}
+            type="button"
+            className="appearance-textfield bg-gray-950 hover:bg-gray-900 border border-gray-800 p-3 h-11 rounded-s-lg"
+          >
+            <FiMinus />
+          </button>
+          <input
+            aria-labelledby={`counter-${name}-label`}
+            name={name}
+            value={count}
+            onChange={() => {}}
+            type="number"
+            required
+            // Informere brugeren opdateringen
+            aria-live="polite"
+            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none appearance-textfield disabled-none text-white text-center bg-gray-950 border-y border-gray-800 h-11 py-2.5 w-full"
+          />
+          <button
+            aria-label="Øg Antal Billetter"
+            onClick={addQuantity}
+            type="button"
+            className=" appearance-textfield bg-gray-950 hover:bg-gray-900 border border-gray-800 p-3 h-11 rounded-e-lg"
+          >
+            <FiPlus />
+          </button>
+        </div>
+        <p id={`counter-${name}-label`} className="sr-only">
+          Du kan maks købe {max} billetter.
+        </p>
+      </label>
     </>
   );
 };
