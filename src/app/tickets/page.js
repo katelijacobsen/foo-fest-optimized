b4fded4c8ae89b62922d729e486669c9ab2defd5
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-05 21:40:10 +0100

    move to step 2

--- src/app/tickets/page.js
+++ src/app/tickets/page.js
@@ -22,7 +22,7 @@ const ceasarDressing = Caesar_Dressing({
 // Multistep-form (f.eks. hvis brugeren er gået over de 5min i PaymentFlow).
 //=================//
 const defaultState = {
-  step: 1,
+  step: 2,
   tickets: {
     single: 2,
     vip: 2,
