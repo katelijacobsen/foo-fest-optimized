6c9139f0b4dff4e349ef5ac2dd7754f3c16afa96
Cecilie <cepe0001@stud.kea.dk>
2025-01-06 13:46:33 +0100

    ændret tekst i herosektion

--- src/components/festivalsystem/HeroSection.jsx
+++ src/components/festivalsystem/HeroSection.jsx
@@ -35,19 +35,19 @@ const ThirdHero = ({ text }) => {
         <div className="flex justify-center items-center leading-0">
           {/* https://ui.indie-starter.dev/docs/text-animation */}
           {splittedText.map((current, index) => (
-            <motion.div key={index} ref={ref} variants={pullupVariant} initial="initial" animate={isInView ? "animate" : ""} custom={index} className={`${ceasarDressing.className} text-[26vw] md:text-[18vw]`}>
+            <motion.div key={index} ref={ref} variants={pullupVariant} initial="initial" animate={isInView ? "animate" : ""} custom={index} className={`${ceasarDressing.className} text-[26vw] md:text-[18vw] leading-tight`}>
               {current === " " ? <span></span> : current}
             </motion.div>
           ))}
         </div>
         <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.8 }} animate={{ y: 10 }}>
           <div className="px-4">
-            <h2 className="text-[5vw] md:text-[2vw] md:max-w-[45rem] text-center">Camp som en viking og skrål som en kriger på FooFest Festival.</h2>
+            <h2 className={`${ceasarDressing.className} text-center leading-tight bg-gradient-to-bl from-customPink to-customOrange bg-clip-text text-transparent text-[16vw] md:text-[10vw]`}>FESTIVAL</h2>
           </div>
         </motion.div>
-        <div className="grid place-content-center mt-16">
+        {/* <div className="grid place-content-center mt-16">
           <FaArrowDown className="text-customOrange w-8 h-8 md:w-10 md:h-10 animate-bounce" />
-        </div>
+        </div> */}
       </div>
       <div>
         <MyMarquee />
