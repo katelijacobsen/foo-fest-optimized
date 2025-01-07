16e1f7979b3ee98543a9a30beee38f9e0eda837b
Cecilie <cepe0001@stud.kea.dk>
2025-01-07 15:00:54 +0100

    flyttet url'er fra pages til supabase.js

--- src/app/program/[slug]/page.js
+++ src/app/program/[slug]/page.js
@@ -1,11 +1,11 @@
 import Header from "@/components/global/Header";
 import SingleBand from "@/components/festivalsystem/SingleBand";
 import Footer from "@/components/global/Footer";
+import { getBandBySlug } from "@/lib/supabase";
 
 const Band = async ({ params }) => {
-  const slug = (await params).slug;
-  let response = await fetch(`https://spring-awesome-stream.glitch.me/bands/${slug}`);
-  let data = await response.json();
+  const { slug } = await params;
+  const data = await getBandBySlug(slug);
 
   return (
     <>
