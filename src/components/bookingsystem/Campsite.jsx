5bc1cafdf4f74b2431af218f702525d4fb3f5b6b
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-08 21:02:35 +0100

    change unit

--- src/components/bookingsystem/Campsite.jsx
+++ src/components/bookingsystem/Campsite.jsx
@@ -188,7 +188,7 @@ export default function Campsite({
         <button type="button" onClick={() => setPopupOpen(true)} className=" cursor-pointer text-xs text-gray-400 hover:underline ">Se de forskellige Campingområder</button>
         
         {popupOpen && (
-          <motion.div initial={{ opacity:0}} animate={{opacity:1}} exit={{opacity:0}} className="fixed inset-0 bg-opacity-[25%] bg-black flex justify-center items-center z-100">
+          <motion.div initial={{ opacity:0}} animate={{opacity:1}} exit={{opacity:0}} className="fixed inset-0 bg-opacity-50 bg-black flex justify-center items-center z-100">
             <div className="relative max-w-lg w-full border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md m-8">
             <button onClick={() => setPopupOpen(false)} className="absolute top-2 right-4 text-slate-600 hover:underline" aria-label="Luk Kortet">
               <RxCross2 className="bg-customBlack_5 text-customOrange h-10 w-10 md:ml-0 md:mb-8 border-solid border-[1px] border-customOrange rounded-full p-2"/>
