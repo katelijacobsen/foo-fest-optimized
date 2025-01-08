449a26f6da6ae66555fdfef37b191982fc60dcd7
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-08 21:04:03 +0100

    wrap btn

--- src/components/bookingsystem/Campsite.jsx
+++ src/components/bookingsystem/Campsite.jsx
@@ -185,8 +185,9 @@ export default function Campsite({
             </li>
           ))}
         </ul>
+        <div>
         <button type="button" onClick={() => setPopupOpen(true)} className=" cursor-pointer text-xs text-gray-400 hover:underline ">Se de forskellige Campingområder</button>
-        
+        </div>
         {popupOpen && (
           <motion.div initial={{ opacity:0}} animate={{opacity:1}} exit={{opacity:0}} className="fixed inset-0 bg-opacity-50 bg-black flex justify-center items-center z-100">
             <div className="relative max-w-lg w-full border border-gray-500 bg-customBlack_5 p-4 my-4 rounded-md m-8">
